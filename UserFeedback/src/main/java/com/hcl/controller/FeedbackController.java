136b7d8b0085e32b48f0a83ea522c78c2751b062
Spangevan <ewlson23@gmail.com>
2021-02-06 18:59:28 -0500

    added feedback controller and application properties

---
+++ UserFeedback/src/main/java/com/hcl/controller/FeedbackController.java
@@ -0,0 +1,12 @@
+package com.hcl.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeedbackController {
+	
+	Logger logger = LoggerFactory.getLogger(FeedbackController.class);
+
+}
